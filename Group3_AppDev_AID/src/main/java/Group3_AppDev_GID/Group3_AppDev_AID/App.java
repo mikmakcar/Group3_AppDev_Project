799a491533ef68f3ce8d9a06ca9286f701e1e4d7
mikmakcar <mikhail.carino@mycit.ie>
2014-11-03 10:38:15 +0000

    First Commit

---
+++ Group3_AppDev_AID/src/main/java/Group3_AppDev_GID/Group3_AppDev_AID/App.java
@@ -0,0 +1,13 @@
+package Group3_AppDev_GID.Group3_AppDev_AID;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
